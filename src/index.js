8379000ed864eb37333b20b6e5e3209ab71c5a10
aryang2377 <aryanq102@gmail.com>
2026-03-23 19:59:03 +0530

    api

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port:$ {process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO Db connection failed",err);
+})
 // import express from "express"
 // const app =express()
 // ( async () => {
